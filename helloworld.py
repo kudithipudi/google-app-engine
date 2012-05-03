577211b3bea618b1220057d5d86e1a6f7d683c71
Vinay Kudithipudi <vinay@kudithipudi.org>
2012-05-02 21:53:04 -0500

    First commit

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Hello, Samurai!')
+	  
+class SecondPage(webapp2.RequestHandler):
+  def get(self):
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write('Hello, second!')
+
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
+							  
