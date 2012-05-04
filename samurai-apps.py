197c1d2622e6f0a17dff5d0347223d58603cf313
Vinay Kudithipudi <vinay@kudithipudi.org>
2012-05-03 23:23:53 -0500

    Able to print the user-agent string.

--- samurai-apps.py
+++ samurai-apps.py
@@ -3,15 +3,14 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
-      self.response.out.write('Hello, Samurai!')
+#      self.response.out.write('Hello, Samurai!')
+      self.response.out.write(self.request.headers['User-Agent'])
 	  
 class SecondPage(webapp2.RequestHandler):
   def get(self):
       self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write('Hello, second!')
 
-
 app = webapp2.WSGIApplication([('/', MainPage),
 			       ('/second',SecondPage)],
                               debug=True)
-							  
